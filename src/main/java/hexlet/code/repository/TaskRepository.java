b80f7a9279a7abbb4542fe938e43aeef6684d8b0
Georgii Vlasov <honest.niceman@gmail.com>
2023-12-06 12:32:28 +0400

    Add methods for hexlet checks

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -4,5 +4,8 @@ import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+    Optional<Task> findByName(String name);
 }
